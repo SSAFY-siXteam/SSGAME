a88bc475961c0a5c03e817fe1dfde2c57d0b019c
jiyoon <jiyoon07125@gmail.com>
2022-03-08 11:52:06 +0900

    #S06P22A306-104 Create: 프로젝트 초기 세팅

---
+++ src/main/java/com/sixteam/ssgame/SsgameApplication.java
@@ -0,0 +1,13 @@
+package com.sixteam.ssgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgameApplication.class, args);
+	}
+
+}
