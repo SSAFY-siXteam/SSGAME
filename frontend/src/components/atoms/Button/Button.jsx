6c483ad79907d388576593b17b1d62de9c22f01c
김응주 <dmdwn3979@naver.com>
2022-03-11 13:56:52 +0900

    #S06P22A306-39 Create: Steam Button Add

---
+++ frontend/src/components/atoms/Button/Button.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * Primary UI component for user interaction
+ */
+export function Button({ primary, custom, size, label, img }) {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+
+  // steam log in button
+  if (custom === "STEAM_LOG_IN_BUTTON") {
+    return (
+      <button
+        type="button"
+        className={["storybook-button", `storybook-button--${size}`, mode].join(
+          " "
+        )}
+      >
+        <img src={img} />
+        {label}
+      </button>
+    );
+  }
+  return (
+    <button
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
+    >
+      {label}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary: PropTypes.bool,
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: "medium",
+  onClick: undefined,
+};
