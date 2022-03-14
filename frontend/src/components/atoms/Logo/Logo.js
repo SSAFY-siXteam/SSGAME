c3255ff2e419ec1c83258dcd108d62065707f2fe
unain-dev <daeun84366@gmail.com>
2022-03-14 10:50:17 +0900

    #S06P22A306-6 Test: Storybook Test 코드

---
+++ frontend/src/components/atoms/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../../../assets/img/logo.png";
+import { logoIMG } from "./style";
+
+export default function Logo() {
+  return (
+    <>
+      <img src={logo} />
+    </>
+  );
+}
---
+++ frontend/src/components/atoms/Logo/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import logo from "../../../assets/img/logo.png";
+
+const logoIMG = styled.img`
+  width: 100px;
+`;
+
+export { logoIMG };
