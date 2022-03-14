b006cd7014ea62954f7be1f6c9722e8ff25b2eaa
김은선 <kus0412@naver.com>
2022-03-14 14:10:05 +0900

    #S06P22A306-55 Create: Security 설정을 위한 Member directory setting
    - dto, exception, repository의 .gitkeep은 추후 파일 생성시 삭제 필요

---
+++ backend/src/main/java/com/sixteam/ssgame/api/member/controller/MemberController.java
@@ -0,0 +1,14 @@
+package com.sixteam.ssgame.api.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+@RestController
+public class MemberController {
+
+}
