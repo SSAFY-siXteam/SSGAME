064718b6c3666b20f05a0f3c04996d2f84f4104a
김은선 <kus0412@naver.com>
2022-03-15 03:33:31 +0000

    [BE: 회원관리] #S06P22A306-29 아이디 중복 검사 API 추가

--- backend/src/main/java/com/sixteam/ssgame/api/member/controller/MemberController.java
+++ backend/src/main/java/com/sixteam/ssgame/api/member/controller/MemberController.java
@@ -1,9 +1,18 @@
 package com.sixteam.ssgame.api.member.controller;
 
+import com.sixteam.ssgame.api.member.service.MemberService;
+import com.sixteam.ssgame.global.common.dto.BaseResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -11,4 +20,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
 
+    private final MemberService memberService;
+
+    @GetMapping("/id/{id}/exist")
+    public BaseResponseDto isExistId(@PathVariable String id) {
+        log.debug("아이디 중복 검사 api 호출 - MemberController.isExistId()");
+
+        Integer status = null;
+        String msg = null;
+
+        String regx = "^[a-z]+[0-9a-z]{3,15}$";
+        Pattern pattern = Pattern.compile(regx);
+
+        if (!pattern.matcher(id).matches()) {
+            status = HttpStatus.BAD_REQUEST.value();
+            msg = "ID가 형식에 맞지 않습니다.";
+        } else if (memberService.hasId(id)) {
+            status = HttpStatus.OK.value();
+            msg = "이미 존재하는 ID입니다.";
+        } else {
+            status = HttpStatus.NO_CONTENT.value();
+            msg = "사용할 수 있는 ID입니다.";
+        }
+
+        return BaseResponseDto.builder()
+                .status(status)
+                .msg(msg)
+                .build();
+    }
 }
---
+++ backend/src/main/java/com/sixteam/ssgame/api/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.sixteam.ssgame.api.member.repository;
+
+import com.sixteam.ssgame.api.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsById(String id);
+
+}
