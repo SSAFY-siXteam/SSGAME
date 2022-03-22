f13619b0cb49bc4f9c2759b1d4ffbb33e3f908cf
unain-dev <daeun84366@gmail.com>
2022-03-22 15:41:20 +0900

    #S06P22A306-138 Design: 가장 많이 플레이 한 장르 UI 정렬 수정

--- frontend/src/components/molecules/TopGenreItem/style.js
+++ frontend/src/components/molecules/TopGenreItem/style.js
@@ -9,12 +9,13 @@ export const InfoGrid = styled.div`
   margin-left: 10px;
   display: inline-block;
   width: 150px;
+  text-align: left; // 장르명을 왼쪽 정렬
 `;
 
 export const StyledRatio = styled.div`
   display: inline-block;
   width: 100%;
-  text-align: right;
+  text-align: right; // %를 오른쪽 정렬
 `;
 
 export const TopGenreItemGrid = styled.div`
