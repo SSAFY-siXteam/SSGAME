717c1b66cef749f37875264d04ab9fabd14f74ff
김은선 <kus0412@naver.com>
2022-03-25 01:08:49 +0900

    #S06P22A306-151 Feat: Steam game API로 사용자 정보 받아오는 로직 구현

--- backend/src/main/java/com/sixteam/ssgame/api/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/sixteam/ssgame/api/member/service/MemberServiceImpl.java
@@ -1,6 +1,11 @@
 package com.sixteam.ssgame.api.member.service;
 
 import com.sixteam.ssgame.api.analyze.repository.CategoryRepository;
+import com.sixteam.ssgame.api.game.entity.GameInfo;
+import com.sixteam.ssgame.api.game.entity.MemberGameList;
+import com.sixteam.ssgame.api.game.exception.GameNotFoundException;
+import com.sixteam.ssgame.api.game.repository.GameInfoRepository;
+import com.sixteam.ssgame.api.game.repository.MemberGameListRepository;
 import com.sixteam.ssgame.api.member.dto.request.RequestMemberDto;
 import com.sixteam.ssgame.api.member.dto.response.ResponseLoginMemberDto;
 import com.sixteam.ssgame.api.member.entity.Member;
@@ -35,6 +40,10 @@ public class MemberServiceImpl implements MemberService {
 
     private final CategoryRepository categoryRepository;
 
+    private final MemberGameListRepository memberGameListRepository;
+
+    private final GameInfoRepository gameInfoRepository;
+
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -58,32 +67,34 @@ public class MemberServiceImpl implements MemberService {
         String encryptedPassword = passwordEncoder.encode(requestMemberDto.getPassword());
         log.debug("패스워드 암호화 " + encryptedPassword);
 
-        String steamNickname = null;
-        String avatarUrl = null;
-        boolean isPublic = false;
-        Integer gameCount = -1;
+        Map<String, Object> steamAPIMemberData = new HashMap<>();
+        Map<String, Object> steamAPIGameData = new HashMap<>();
 
-        Map<String, Object> steamAPIData = new HashMap<>();
+        System.out.println("-1");
+
+        String steamID = requestMemberDto.getSteamID();
+        System.out.println("steamID = " + steamID);
         try {
-            steamAPIData = SteamAPIScrap.getMemberData(requestMemberDto.getSteamID());
+            steamAPIMemberData = SteamAPIScrap.getMemberData(steamID);
+            System.out.println("[ member load ]");
+            steamAPIGameData = SteamAPIScrap.getGameData(steamID);
         } catch (IOException | ParseException | APIConnectionException e) {
             e.printStackTrace();
         }
 
-        steamNickname = (String) steamAPIData.get("steamNickname");
-        avatarUrl = (String) steamAPIData.get("avatarUrl");
-        isPublic = (boolean) steamAPIData.get("isPublic");
+        System.out.println("0");
 
         Member savedMember = memberRepository.save(Member.builder()
                 .ssgameId(requestMemberDto.getSsgameId())
                 .password(encryptedPassword)
                 .email(requestMemberDto.getEmail())
-                .steamID(requestMemberDto.getSteamID())
-                .steamNickname(steamNickname)
-                .avatarUrl(avatarUrl)
-                .isPublic(isPublic)
-                .gameCount(gameCount)
+                .steamID(steamID)
+                .steamNickname((String) steamAPIMemberData.get("steamNickname"))
+                .avatarUrl((String) steamAPIMemberData.get("avatarUrl"))
+                .isPublic((boolean) steamAPIMemberData.get("isPublic"))
+                .gameCount((Long) steamAPIGameData.get("gameCount"))
                 .build());
+        System.out.println("1");
 
         List<String> preferredCategories = requestMemberDto.getPreferredCategories();
         for (String preferredCategory : preferredCategories) {
@@ -92,6 +103,30 @@ public class MemberServiceImpl implements MemberService {
                     .category(categoryRepository.findByCategoryName(preferredCategory))
                     .build());
         }
+
+        Map<Long, Long> memberGameList = (Map<Long, Long>) steamAPIGameData.get("memberGameList");
+        System.out.println("2");
+        for (Long steamAppid: memberGameList.keySet()) {
+            // 게임 정보를 db에 저장한 이후에 사용
+//            GameInfo gameInfo = gameInfoRepository.findBySteamAppid(steamAppid);
+//            if (gameInfo == null) {
+//                throw new GameNotFoundException(steamAppid);
+//            }
+            // steam app id에 해당하는 게임 저장
+            GameInfo gameInfo = gameInfoRepository.save(GameInfo.builder()
+                    .gameName("beta test")
+                    .steamAppid(steamAppid)
+                    .isFree(true)
+                    .build());
+            // 회원가입 하고 나서 수정할 때는... 또 다른 로직이 필요함...
+            // 새로 추가한 게임이나 기존 게임에서 플레이 시간만 업데이트 하는 로직
+            memberGameListRepository.save(MemberGameList.builder()
+                    .member(savedMember)
+                    .gameInfo(gameInfo)
+                    .memberPlayTime(memberGameList.get(steamAppid))
+                    .build());
+        }
+        System.out.println("3");
     }
 
     @Override
--- backend/src/main/java/com/sixteam/ssgame/global/common/steamapi/SteamAPIScrap.java
+++ backend/src/main/java/com/sixteam/ssgame/global/common/steamapi/SteamAPIScrap.java
@@ -52,7 +52,7 @@ public class SteamAPIScrap {
 
             responseData.put("steamNickname", playerInfoJson.get("personaname"));
             responseData.put("avatarUrl", playerInfoJson.get("avatarfull"));
-            responseData.put("isPublic", (int) playerInfoJson.get("communityvisibilitystate") == 3);
+            responseData.put("isPublic", (Long) playerInfoJson.get("communityvisibilitystate") == 3L);
         } else {
             throw new APIConnectionException("[Error] api connection url : " + urlBuilder.toString());
         }
@@ -60,7 +60,52 @@ public class SteamAPIScrap {
         return responseData;
     }
 
-    private static String getResponse(HttpsURLConnection conn, boolean isSuccess) throws IOException {
+    public static Map<String, Object> getGameData(String steamID) throws IOException, ParseException {
+
+        Map<String, Object> responseData = new HashMap<>();
+        StringBuilder urlBuilder = new StringBuilder();
+        urlBuilder
+                .append(OWNED_GAMES_BASE_URL)
+                .append("?").append(encode("key", UTF_8)).append(EQUAL).append(KEY)
+                .append("&").append(encode("steamid", UTF_8)).append(EQUAL).append(steamID)
+                .append("&").append(encode("format", UTF_8)).append(EQUAL).append(FORMAT);
+
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = getHttpURLConnection(url);
+
+        int responseCode = conn.getResponseCode();
+        boolean isSuccess = 200 <= responseCode && responseCode <= 300;
+        String response = getResponse(conn, isSuccess);
+
+        if (isSuccess) {
+            JSONParser parser = new JSONParser();
+            JSONObject totalInfoJson = (JSONObject) ((JSONObject) parser.parse(response)).get("response");
+            JSONArray gameInfoJsons = (JSONArray) totalInfoJson.get("games");
+
+            if (gameInfoJsons.size() == 0) {
+                // 구매한 게임이 없는 사용자 존재 -> exception 말고 다른 처리 필요
+                throw new InvalidSteamIDException("invalid steam ID : " + steamID);
+            }
+
+            System.out.println("game_count = " + totalInfoJson.get("game_count"));
+            System.out.println("gameInfoJsons.size() = " + gameInfoJsons.size());
+
+            Map<Long, Long> memberGameList = new HashMap<>();
+            for (int i = 0; i < gameInfoJsons.size(); i++) {
+                JSONObject gameInfoJson = (JSONObject) gameInfoJsons.get(i);
+                memberGameList.put((Long) gameInfoJson.get("appid"), (Long) gameInfoJson.get("playtime_forever"));
+            }
+
+            responseData.put("gameCount", totalInfoJson.get("game_count"));
+            responseData.put("memberGameList", memberGameList);
+        } else {
+            throw new APIConnectionException("[Error] api connection url : " + urlBuilder);
+        }
+
+        return responseData;
+    }
+
+    private static String getResponse(HttpURLConnection conn, boolean isSuccess) throws IOException {
 
         BufferedReader br;
         if (isSuccess) {
