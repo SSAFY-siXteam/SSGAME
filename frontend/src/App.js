a717191fd9a0c961425497e11dfc210863b13696
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2022-03-27 16:54:05 +0900

    #S06P22A306-36 : Feat Backend Api 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,30 @@ import SignUpPage from "./components/pages/SignUpPage/SignUpPage";
 import AnalyzePage from "./components/pages/AnalyzePage/AnalyzePage";
 import MyPage from "./components/pages/MyPage/MyPage";
 import { setCookie, getCookie, removeCookie } from "./utils/cookie";
+import { signIn } from "./apis/user";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const onLogIn = async () => {
-    // console.log(userId, userPassword, "clicked");
+  const onLogIn = (userId, userPassword) => {
+    console.log(userId, userPassword, "clicked");
+    if (userId === undefined || userId.length === 0) {
+      alert("아이디를 입력해주세요");
+    } else if (userPassword === undefined || userPassword.length === 0) {
+      alert("비밀번호를 입력해주세요");
+    } else {
+      signIn({ ssgameId: userId, password: userPassword }).then((res) => {
+        // console.log(res);
+        if (res.data.status === 200) {
+          console.log("성공");
+          setCookie("userToken", "testtestttttest", {
+            path: "/",
+          });
+          setIsLoggedIn(true);
+        } else {
+          alert("실패");
+          console.log(res);
+        }
+      });
+    }
     // const jwtToken = await signIn({ ssgameId: userId, password: userPassword });
     // if (jwtToken.status === 200) {
     //   setCookie("userToken", jwtToken.data.jwtToken, {
@@ -19,11 +39,8 @@ function App() {
     // } else {
     //   alert("error");
     // }
-    setCookie("userToken", "testtestttttest", {
-      path: "/",
-    });
+
     // 성공시
-    setIsLoggedIn(true);
   };
   const onLogOut = () => {
     removeCookie("userToken");
--- frontend/src/apis/user.js
+++ frontend/src/apis/user.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //env 이관
-const URL = "http://localhost:3000/api/v1";
+const URL = "http://localhost:8080/api/v1";
 
 // Spring 서버 ID 로그인
 export function signIn(user) {
