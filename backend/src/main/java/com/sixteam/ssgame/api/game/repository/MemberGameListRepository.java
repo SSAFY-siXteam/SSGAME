132fdc3c6bc817bbf00fa21acc09b10313e57774
김은선 <kus0412@naver.com>
2022-03-28 01:22:29 +0900

    #S06P22A306-25 Fix: 복합키 Repository에서 호출하는 방식 변경
    - 기존 : 각 entity의 seq
    - 변경 : 각 entity 자체

--- backend/src/main/java/com/sixteam/ssgame/api/game/repository/MemberGameListRepository.java
+++ backend/src/main/java/com/sixteam/ssgame/api/game/repository/MemberGameListRepository.java
@@ -1,7 +1,12 @@
 package com.sixteam.ssgame.api.game.repository;
 
+import com.sixteam.ssgame.api.game.entity.Game;
 import com.sixteam.ssgame.api.game.entity.MemberGameList;
+import com.sixteam.ssgame.api.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberGameListRepository extends JpaRepository<MemberGameList, Long> {
+
+    MemberGameList findByMemberAndGame(Member member, Game game);
+
 }
