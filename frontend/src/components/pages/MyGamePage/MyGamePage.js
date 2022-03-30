dc275a238ad8474345c9ecad50d60af6ef8d0359
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2022-03-30 19:42:37 +0900

    #S06P22A306-75 Feat: 게임 평가 목록 api 연동 및 기능 구현 완

--- frontend/src/components/pages/MyGamePage/MyGamePage.js
+++ frontend/src/components/pages/MyGamePage/MyGamePage.js
@@ -1,34 +1,57 @@
 import React, { useEffect, useState } from "react";
 import MyGameTemplate from "../../templates/MyGameTemplate/MyGameTemplate";
 import SelectBox from "../../atoms/SelectBox/SelectBox";
+import { getGame1 } from "../../../apis/game";
+import { getCookie } from "../../../utils/cookie";
+//test
 
 const MyGamePage = () => {
+  const [gameList, setGameList] = useState([]);
+  const [param, setParam] = useState({
+    page: 1,
+    size: 10,
+    sort: "playtime",
+    filter: false,
+  });
+  const [debounceState, setDebounceState] = useState(false);
+
+  useEffect(() => {
+    getGame1(getCookie("SSGAME_USER_TOKEN"), param).then((res) => {
+      console.log(res);
+      console.log(res.data.data.myGameInfos);
+      setGameList(res.data.data.myGameInfos);
+    });
+  }, [debounceState]);
+  const onChangeSelectBox = (e) => {
+    console.log(e.target.value);
+    param.sort = e.target.value;
+    setParam(param);
+    setDebounceState(!debounceState);
+  };
   const onInput = (e) => {
     console.log(e.target.value);
+    const search = e.target.value;
+    setParam(Object.assign(param, { search: search }));
+    setDebounceState(!debounceState);
   };
   const onChangeCheck = () => {
-    console.log("a");
+    console.log("check");
+    param.filter = !param.filter;
+    setParam(param);
+    setDebounceState(!debounceState);
   };
   const args = {
     onInput: onInput,
     onChangeCheck: onChangeCheck,
     selectBox: SelectBox({
       options: [
-        { value: "option1", name: "옵션1" },
-        { value: "option2", name: "옵션2" },
-        { value: "option3", name: "옵션3" },
+        { value: "playtime", name: "playtime" },
+        { value: "dic", name: "사전순" },
+        { value: "rating", name: "별점순" },
       ],
+      onChangeSelectBox: onChangeSelectBox,
     }),
-    gameList: [
-      { test: "test" },
-      { test: "test" },
-      { test: "test" },
-      { test: "test" },
-      { test: "test" },
-      { test: "test" },
-      { test: "test" },
-      { test: "test" },
-    ],
+    gameList: gameList,
   };
 
   return (
