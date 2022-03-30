a44a2f063d6dde565cd7e263fef66158eb9af0bd
jiyoon <jiyoon07125@gmail.com>
2022-03-30 15:04:33 +0900

    #S06P22A306-9 Create: 추천게임리스트 조회 API 구현

--- backend/src/main/java/com/sixteam/ssgame/api/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/sixteam/ssgame/api/member/service/MemberServiceImpl.java
@@ -103,13 +103,14 @@ public class MemberServiceImpl implements MemberService {
             GameInfo gameInfo = gameInfoRepository.findBySteamAppid(steamAppid);
             // steam app id에 해당하는 게임 저장
             if (gameInfo == null) {
+                continue;
                 // 게임 정보를 db에 저장한 이후에 사용
 //                throw new CustomException(steamAppid, GAME_NOT_FOUND);
-                gameInfo = gameInfoRepository.save(GameInfo.builder()
-                        .gameName("beta test")
-                        .steamAppid(steamAppid)
-                        .isFree(true)
-                        .build());
+//                gameInfo = gameInfoRepository.save(GameInfo.builder()
+//                        .gameName("beta test")
+//                        .steamAppid(steamAppid)
+//                        .isFree(true)
+//                        .build());
             }
             // 회원가입 하고 나서 수정할 때는... 또 다른 로직이 필요함...
             // 새로 추가한 게임이나 기존 게임에서 플레이 시간만 업데이트 하는 로직
