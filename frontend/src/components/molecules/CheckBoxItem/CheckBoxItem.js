22012aaeaa8e2c2b28d9eccd907def13344f5e3f
김응주 <dmdwn3979@naver.com>
2022-03-30 07:02:34 +0000

    [FE: 회원정보관리] #S06P22A306-60 회원 정보 조회 Api 및 기능 구현

--- frontend/src/components/molecules/CheckBoxItem/CheckBoxItem.js
+++ frontend/src/components/molecules/CheckBoxItem/CheckBoxItem.js
@@ -3,13 +3,14 @@ import PropTypes from "prop-types";
 import { CheckBox } from "../../atoms/CheckBox/CheckBox";
 import { Label } from "../../atoms/Label/Label";
 // checkbox list 객체
-export function CheckBoxItem({ list, id, onChangeCheckBox }) {
+export function CheckBoxItem({ list, id, onChangeCheckBox, checked }) {
   return (
     <>
       <CheckBox
         id={id}
         label={list.label}
         onChangeCheckBox={onChangeCheckBox}
+        checked={checked}
       />
       <Label
         content={list.content}
