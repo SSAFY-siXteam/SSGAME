a462bc3da0fb90abc42c6d7e9b9d265c099699a6
unain-dev <daeun84366@gmail.com>
2022-03-31 14:05:58 +0900

    #S06P22A306-18 Feat: 문자 타이핑 애니메이션 적용

--- frontend/src/components/organisms/Fade/FadeModule.js
+++ frontend/src/components/organisms/Fade/FadeModule.js
@@ -2,17 +2,18 @@ import React from "react";
 import Fade from "react-reveal/Fade";
 import Bounce from "react-reveal/Bounce";
 import { CenterWrapper, ViewWrapper } from "./style";
+import Typist from "react-typist";
 
 const FadeModule = () => {
   return (
     <div>
       <CenterWrapper>
-        <Bounce>
+        <Typist>
           <ViewWrapper>
             <h1>Welcome, WE ARE SSAGAME.</h1>
             <h2>맞춤형 게임 추천 및 성향 분석 사이트 SSGAME입니다.</h2>
           </ViewWrapper>
-        </Bounce>
+        </Typist>
         <Fade left>
           <ViewWrapper>
             <h2>내가 매긴 별점을 기반으로,</h2>
@@ -21,6 +22,11 @@ const FadeModule = () => {
         <Fade right>
           <ViewWrapper>
             <h2>나에게 딱 맞는 게임을 추천받고 싶을 때</h2>
+          </ViewWrapper>
+        </Fade>
+        <Fade top>
+          <ViewWrapper>
+            <h2>내 게임 성향을 분석해서 볼 수 있습니다.</h2>
           </ViewWrapper>
         </Fade>
       </CenterWrapper>
