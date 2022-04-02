3b6cf7cddb2499d9f801029e3d4c77fee42ed5f4
김은선 <kus0412@naver.com>
2022-04-02 17:51:27 +0900

    #S06P22A306-69 Remove: RequestUpdateMemberSteamIDDto 제거

--- backend/src/main/java/com/sixteam/ssgame/api/member/controller/MemberController.java
+++ backend/src/main/java/com/sixteam/ssgame/api/member/controller/MemberController.java
@@ -5,7 +5,6 @@ import com.sixteam.ssgame.api.member.dto.MemberDto;
 import com.sixteam.ssgame.api.member.dto.request.RequestLoginMemberDto;
 import com.sixteam.ssgame.api.member.dto.request.RequestMemberDto;
 import com.sixteam.ssgame.api.member.dto.request.RequestUpdateMemberDto;
-import com.sixteam.ssgame.api.member.dto.request.RequestUpdateMemberSteamIDDto;
 import com.sixteam.ssgame.api.member.dto.response.ResponseMemberGamePageDto;
 import com.sixteam.ssgame.api.member.service.MemberService;
 import com.sixteam.ssgame.global.common.auth.CustomUserDetails;
@@ -275,7 +274,7 @@ public class MemberController {
     }
 
     @PutMapping("/steamID")
-    public BaseResponseDto updateSteamID(Authentication authentication, @Valid @RequestParam RequestUpdateMemberSteamIDDto requestUpdateMemberSteamIDDto, Errors errors) {
+    public BaseResponseDto updateSteamID(Authentication authentication, @Valid @RequestBody String steamID, Errors errors) {
         log.info("Called API: {}", LogUtil.getClassAndMethodName());
 
         Integer status = null;
