f8d74f58676347681b40838bcb7834f2c8b20baf
unain-dev <daeun84366@gmail.com>
2022-04-04 09:44:17 +0900

    #S06P22A306-167 Feat: 로그인 성공 시 가중치 업데이트 api 호출

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import GameInfoPage from "./components/pages/GameInfoPage/GameInfoPage";
 import { signIn } from "./apis/user";
 import RecommendPage from "./components/pages/RecommendPage/RecommendPage";
 import MyGamePage from "./components/pages/MyPage/MyGamePage/MyGamePage";
+import { updateGameAnalyzation } from "./apis/analyze";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -35,6 +36,19 @@ function App() {
             path: "/",
           });
           setIsLoggedIn(true);
+          updateGameAnalyzation(
+            {
+              headers: {
+                Authorization: `Bearer ` + res.data.data.jwtToken,
+              },
+            },
+            (response) => {
+              console.log(response);
+            },
+            (error) => {
+              console.log(error);
+            }
+          );
         } else {
           alert("실패");
           console.log(res);
