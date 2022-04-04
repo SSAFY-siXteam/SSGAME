382395aa9bee91ed9b3363da9c63cd226912e333
unain-dev <daeun84366@gmail.com>
2022-04-04 14:27:14 +0900

    #S06P22A306-38 Feat: game 별점 렌더링

--- frontend/src/components/organisms/GameInfo/GameInfo.js
+++ frontend/src/components/organisms/GameInfo/GameInfo.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import InfoText from "../../atoms/Text/InfoText/InfoText";
 import Title from "../../atoms/Title/Title";
 import GameInfoItem from "../../molecules/GameInfoItem/GameInfoItem";
-import RatingStar from "../../atoms/RatingStart/RatingStar";
+import RatingStar from "../../atoms/RatingStar/RatingStar";
 
 const GameInfo = ({ gameInfo }) => {
+  // gameInfo.memberGameRating
+  const [starChanged, setStarChanged] = useState(false);
+
+  const onStarChange = () => {
+    setStarChanged(!starChanged);
+  };
+
   return (
     <div>
       <Title title={gameInfo.gameName} />
-      {gameInfo.isPlayed && <RatingStar />}
       <InfoText text={gameInfo.shortDescriptionKr} />
       {/* {gameInfo.isPlayed && } */}
       <GameInfoItem title="내 게임 평가" text={gameInfo.memberGameRating} />
       {/* 별점 */}
-
+      <RatingStar
+        content={{ ...gameInfo, memberGameRating: 4 }}
+        onStarChange={onStarChange}
+      />
       <GameInfoItem title="게임 평점" text={gameInfo.averageRating} />
       <GameInfoItem title="게임 장르" text={gameInfo.genres + " "} />
     </div>
