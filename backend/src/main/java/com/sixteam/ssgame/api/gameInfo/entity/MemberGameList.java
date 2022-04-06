4015cf12f5622ca77f1832579c240d5d20ae1a38
김경현 <kyoung1548@gmail.com>
2022-04-06 16:15:29 +0900

    HotFix: 플레이한 게임 목록 불러오기 수정

--- backend/src/main/java/com/sixteam/ssgame/api/gameInfo/entity/MemberGameList.java
+++ backend/src/main/java/com/sixteam/ssgame/api/gameInfo/entity/MemberGameList.java
@@ -51,6 +51,17 @@ public class MemberGameList {
         this.memberGameRating = memberGameRating;
     }
 
+    @Override
+    public String toString() {
+        return "MemberGameList{" +
+                "memberGameListSeq=" + memberGameListSeq +
+                ", member=" + member +
+                ", gameInfo=" + gameInfo +
+                ", memberPlayTime=" + memberPlayTime +
+                ", memberGameRating=" + memberGameRating +
+                '}';
+    }
+
     public void updateMemberGameRating (Integer memberGameRating){
         this.memberGameRating = memberGameRating;
     }
