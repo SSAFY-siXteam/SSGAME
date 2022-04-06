b0b92f3652711bfb4a389ff5d87c860ff65eba0d
dsaeun <daeun84366@gmail.com>
2022-04-06 22:45:25 +0900

    Fix: 게임 추천 204 오류 alert 메세지 수정

--- frontend/src/apis/recommend.js
+++ frontend/src/apis/recommend.js
@@ -6,6 +6,10 @@ async function getRecommendGames(header, success, fail) {
     let response = await axios.get(URL + `recommend`, header);
     if (response.data.status == 200) {
       success(response);
+    } else if (response.data.status == 204) {
+      alert(
+        "평가한 게임이 없어 게임을 추천해드릴 수 없어요 :( 성향에 맞는 게임을 몇가지 골라 플레이 후 서비스 이용 해보시겠어요?"
+      );
     } else if (response.data.status !== 200) {
       alert("게임 목록을 가져올 수 없습니다. 다시 시도해 주세요.");
     }
