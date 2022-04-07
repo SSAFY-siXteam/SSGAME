c3e350c5df6db08b50abbdf03bcf78bae2787643
unain-dev <daeun84366@gmail.com>
2022-04-07 09:59:01 +0900

    Fix: alert 메세지 수정

--- frontend/src/apis/analyze.js
+++ frontend/src/apis/analyze.js
@@ -7,7 +7,9 @@ export async function getAnalyzeGraph(header, success, fail) {
     if (response.data.status == 200) {
       success(response);
     } else if (response.data.status == 204) {
-      alert("플레이한 게임이 없어서 성향을 파악하기 힘들어요 :( 게임을 몇 개 플레이 한 뒤 서비스를 이용해보시겠어요?")
+      alert(
+        "플레이한 게임이 없어서 성향을 파악하기 힘들어요 :( \n 게임을 몇 개 플레이 한 뒤 서비스를 이용해보시겠어요?"
+      );
     } else if (response.data.status !== 200) {
       throw new Error("analyzation is failed");
     }
