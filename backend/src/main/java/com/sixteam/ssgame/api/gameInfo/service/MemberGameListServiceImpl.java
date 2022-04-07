0f7da1435f5971ecb6445fd489a09b289a1ccbab
김은선 <kus0412@naver.com>
2022-04-07 04:19:40 +0000

    [BE: FIX] member game list 호출시 사용자 정보가 없는 경우 exception 처리 추가

--- backend/src/main/java/com/sixteam/ssgame/api/gameInfo/service/MemberGameListServiceImpl.java
+++ backend/src/main/java/com/sixteam/ssgame/api/gameInfo/service/MemberGameListServiceImpl.java
@@ -3,6 +3,11 @@ package com.sixteam.ssgame.api.gameInfo.service;
 import com.sixteam.ssgame.api.member.dto.MemberGameDto;
 import com.sixteam.ssgame.api.member.dto.response.ResponseMemberGamePageDto;
 import com.sixteam.ssgame.api.gameInfo.repository.MemberGameListRepository;
+import com.sixteam.ssgame.api.member.entity.Member;
+import com.sixteam.ssgame.api.member.repository.MemberRepository;
+import com.sixteam.ssgame.global.common.auth.CustomUserDetails;
+import com.sixteam.ssgame.global.common.util.LogUtil;
+import com.sixteam.ssgame.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +15,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static com.sixteam.ssgame.global.error.dto.ErrorStatus.MEMBER_NOT_FOUND;
 
 @Transactional(readOnly = true)
 @Slf4j
@@ -19,8 +26,16 @@ public class MemberGameListServiceImpl implements MemberGameListService {
 
     private final MemberGameListRepository memberGameListRepository;
 
+    private final MemberRepository memberRepository;
+
     @Override
-    public ResponseMemberGamePageDto getResponseMemberGamePageDto(Long memberSeq, Pageable pageable, boolean filter, String search) {
+    public ResponseMemberGamePageDto getResponseMemberGamePageDto(CustomUserDetails details, Pageable pageable, boolean filter, String search) {
+
+
+        String ssgameId = details.getUsername();
+        Member member = memberRepository.findBySsgameId(ssgameId)
+                .orElseThrow(() -> new CustomException(LogUtil.getElement(), MEMBER_NOT_FOUND));
+        Long memberSeq = member.getMemberSeq();
 
         Integer totalCnt = memberGameListRepository.countMemberGameListByFilterAndSearch(memberSeq, filter, search);
         Integer totalPage = (int) Math.ceil((double) totalCnt / pageable.getPageSize());
