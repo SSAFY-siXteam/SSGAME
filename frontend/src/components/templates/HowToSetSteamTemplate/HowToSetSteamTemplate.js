878574dd9c32e8acd30b9089e2c5e720f0fb426e
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2022-04-07 14:06:23 +0900

    !HOTFIX: Routing 설정 및 steam 공개 설정 component 생성

---
+++ frontend/src/components/templates/HowToSetSteamTemplate/HowToSetSteamTemplate.js
@@ -0,0 +1,29 @@
+import React from "react";
+import set_steam_public_1 from "../../../assets/img/howto/set_steam_public_1.PNG";
+import set_steam_public_2 from "../../../assets/img/howto/set_steam_public_2.PNG";
+import set_steam_public_3 from "../../../assets/img/howto/set_steam_public_3.PNG";
+import set_steam_public_4 from "../../../assets/img/howto/set_steam_public_4.PNG";
+import { ExplanationDiv, PictureDiv } from "./styles";
+export default function HowToSetSteamTemplate() {
+  return (
+    <div>
+      <ExplanationDiv>
+        1. 스팀에 로그인을 해주신 후, 우측 상단 자신의 닉네임을 클릭해주세요
+        <PictureDiv src={set_steam_public_1} style={{ width: "100%" }} />
+      </ExplanationDiv>
+      <ExplanationDiv>
+        2. 우측 하단의 내 프로필 편집에 접근 해 주세요
+        <PictureDiv src={set_steam_public_2} style={{ width: "100%" }} />
+      </ExplanationDiv>
+      <ExplanationDiv>
+        3. 좌측 하단의 공개 설정에 접근 해 주세요
+        <PictureDiv src={set_steam_public_3} style={{ width: "100%" }} />
+      </ExplanationDiv>
+      <ExplanationDiv>
+        {" "}
+        4. 기본 정보와 프로필을 모두 공개로 설정해주세요
+        <PictureDiv src={set_steam_public_4} style={{ width: "100%" }} />
+      </ExplanationDiv>
+    </div>
+  );
+}
